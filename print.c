d3d5231e70d03c68d0b22c2825e40d59fd220bdc
nisha-khare <nishakhare9628@gmail.com>
2023-05-31 11:40:44 +0530

    first commit

---
+++ print.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j,k;
+int space=4;
+
+for (int i = 1; i <=5; i++)//row
+{
+    for (  = 0; i < count; i++)
+    {
+    for (int j = 1; j < count; i++)
+    {
+        /* code */
+    }
+        
+    }
+    
+}
+
+}
